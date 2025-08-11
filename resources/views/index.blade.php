2dad12da4ad48db5704425a7afa0688ca3c1376f
AndreaWeb01 <andreawebfac@gmail.com>
2025-08-11 11:55:54 +0000

    Mise a jour du composer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -155,7 +155,7 @@
                         <p class="offre normal-text">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
                             ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                            <!-- laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in. -->
+                            
                         </p>
                     </div>
                 
